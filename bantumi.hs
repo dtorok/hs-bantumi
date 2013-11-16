2b57387b4e23d43e2c6884759d1df94003cb15d3
Daniel Torok <daniel.torok@prezi.com>
2013-11-16 23:18:44 +0100

    Almost done the logic

--- bantumi.hs
+++ bantumi.hs
@@ -1,3 +1,5 @@
+import Debug.Trace
+
 size :: Int
 size = 6
 
@@ -33,22 +35,30 @@ getState :: Board -> (Int, Int)
 getState board = (stonesAt size board, stonesAt (size * 2 + 1) board)
 
 moveStones :: Int -> Board -> Board
-moveStones pos board@Board{holes=holes} = board{holes=(distribute (pos+1) value) . (emptyAt pos) $ holes}
+moveStones pos board@Board{holes=holes} = distribute (pos+1) value board{holes=emptyAt pos holes}
 	where
 		value = holes `at` pos
 
-		distribute :: Int -> Int -> [Int] -> [Int]
-		distribute _ 0 hs = hs
-		distribute p v hs | p >= ( 2 * size + 2) = distribute 0 v hs
-		distribute p v hs = distribute (p + 1) (v - 1) hs' where
-			hs' = modifyAt (+1) p hs
-		
-		emptyAt = modifyAt (\ _ -> 0)
+		distribute :: Int -> Int -> Board -> Board
+		distribute _ 0 board = board
+		distribute p v board | p >= ( 2 * size + 2) = distribute 0 v board
+		distribute p v board@Board{holes=holes} =  distribute (p + 1) (v - 1) board{holes=hs'} where
+			hs' = modifyAt (+1) p holes
+			changePlayerIfNeeded board | and [v == 1, isColl p] = board
+			changePlayerIfNeeded board | and [v == 1, (stonesAt p board) /= 1] = changePlayer board
+			changePlayerIfNeeded board = board
 
 		other :: Int -> Int
 		other 0 = 1
 		other 1 = 0
-		other _ = undefined		
+		other _ = undefined
+
+normalizePos :: Int -> Int
+normalizePos pos = pos `mod` (2 * (size + 1))
+
+opositePos :: Int -> Int
+opositePos pos | pos < size = size + (size - pos)
+opositePos pos | pos > size = size * 2 - pos -- size - 1 - pos + size + 1
 
 changePlayer :: Board -> Board
 changePlayer board@Board{player=player} = board{player=player'}
@@ -60,6 +70,20 @@ changePlayer board@Board{player=player} = board{player=player'}
 stonesAt :: Int -> Board -> Int
 stonesAt pos Board{holes=holes} = holes `at` pos
 
+collectStones :: Int -> Board -> Board
+collectStones pos board = board{holes=holes'} where
+	holes' = addToColl . emptyPos . holes $ board
+
+	emptyPos = modifyAt (\ _ -> 0) pos
+	addToColl = modifyAt (+ stones) collPos
+
+	pl = player board
+	stones = stonesAt pos board
+	collPos = pl * (size + 1) + size
+
+emptyAt :: Int -> [Int] -> [Int]
+emptyAt = modifyAt (\ _ -> 0)
+
 modifyAt :: (Int -> Int) -> Int -> [Int] -> [Int]
 modifyAt _ _ [] = undefined
 modifyAt f 0 (x:xs) = ((f x):xs)
@@ -67,6 +91,10 @@ modifyAt f p (x:xs) = (x:(modifyAt f (p-1) xs))
 
 isColl :: Int -> Bool
 isColl pos = or [pos == size, pos == 2 * size + 1]
+
+belongsToPlayer :: Int -> Int -> Bool
+belongsToPlayer pos player = and [pos >= offset, pos < offset + size] where
+	offset = player * (size + 1)
 
 
 at :: [a] -> Int -> a
@@ -90,28 +118,61 @@ validateInput ch board@Board{player=player} = case and [numberChosen, ownHoleCho
 		holeHasStones = stonesAt realPos board > 0
 
 
+makeStep :: Int -> Board -> (Int, Board)
+makeStep pos board = (makeResult . changePlayerRule . cleanupAtTheEnd . stealStonesRule . moveStonesRule) board
+	where
+		lasthole = normalizePos $ pos + (stonesAt pos board)
+
+		moveStonesRule board' = moveStones pos board'
+
+		stealStonesRule board' = if and [playersHole, lastWasEmpty] then newboard else board' where
+			playersHole = belongsToPlayer lasthole (player board)
+			lastWasEmpty = stonesAt lasthole board' == 1
+			newboard = (collectStones $ opositePos lasthole) . 
+			           (collectStones lasthole) $ board'
+
+		changePlayerRule board' = case isColl lasthole of
+										True -> board'
+										False -> changePlayer board'
+
+		cleanupAtTheEnd board' = board' -- TODO -- if or [allHolesEmpty 0, allHolesEmpty 1]
+
+		makeResult :: Board -> (Int, Board)
+		makeResult board' = (-1, board')
+
+
 gameLoop :: Board -> IO ()
 gameLoop board = do
 		putStr "\n"
 		printBoard board
+		putStr $ "Next player: player" ++ (show $ (player board) + 1) ++ "\n"
 
 		ch <- getInput
 
-		let board' = case validateInput ch board of
-			Nothing -> board
-			Just pos -> moveStones pos board
+		let (winner, newboard) = case validateInput ch board of
+			Nothing -> (-1, board)
+			Just pos -> makeStep pos board
 
-		gameLoop board'
+		gameLoop newboard
+
 
 test :: IO ()
 test = do
-		_ <- testValidateInput
-		_ <- testIsColl
+		_ <- test "validateInput" testValidateInput
+		_ <- test "isColl" testIsColl
+		_ <- test "normalizePos" testNormalizePos
+		_ <- test "belongsToPlayer" testBelongsToPlayer
+		_ <- test "opositePos" testOpositePos
 		putStr "Done.\n"
 	where
+		test :: String -> IO () -> IO ()
+		test title testfunction = do
+			putStr $ "Testing: " ++ title
+			testfunction
+			putStr "\n"
+
 		testValidateInput :: IO ()
 		testValidateInput = do
-			putStr "Testing: validateInput"
 			let board1 = ((moveStones 11) . (moveStones 5) . (moveStones 4)) createBoard
 			let board2 = changePlayer board1
 			assertEquals (validateInput '0' board1) (Just 0)
@@ -122,18 +183,43 @@ test = do
 			assertEquals (validateInput '0' board2) (Just 7)
 			assertEquals (validateInput '4' board2) (Nothing) -- empty
 			assertEquals (validateInput '6' board2) (Nothing) -- overflow
-			putStr "\n"
 
 		testIsColl :: IO ()
 		testIsColl = do
-			putStr "Testing: isColl"
 			assertEquals (isColl $ 0)            False
 			assertEquals (isColl $ size - 1)     False
 			assertEquals (isColl $ size)         True
 			assertEquals (isColl $ size + 1)     False
 			assertEquals (isColl $ 2 * size)     False
 			assertEquals (isColl $ 2 * size + 1) True
-			putStr "\n"
+
+		testNormalizePos :: IO ()
+		testNormalizePos = do
+			let fullsize = 2 * (size + 1)
+			assertEquals (normalizePos 0) 0
+			assertEquals (normalizePos 1) 1
+			assertEquals (normalizePos size) size
+			assertEquals (normalizePos $ fullsize - 1) (fullsize - 1)
+			assertEquals (normalizePos fullsize) 0
+			assertEquals (normalizePos $ fullsize + 1) 1
+
+		testBelongsToPlayer :: IO ()
+		testBelongsToPlayer = do
+			assertEquals (belongsToPlayer 0 0)               True
+			assertEquals (belongsToPlayer 1 0)               True
+			assertEquals (belongsToPlayer (size-1) 0)        True
+			assertEquals (belongsToPlayer  size    0)        False
+			assertEquals (belongsToPlayer (size+1) 0)        False
+			assertEquals (belongsToPlayer (size+1) 1)        True
+			assertEquals (belongsToPlayer (size+1+size-1) 1) True
+			assertEquals (belongsToPlayer (size+1+size) 1)   False
+
+		testOpositePos :: IO ()
+		testOpositePos = do
+			assertEquals (opositePos 0) (size + size)
+			assertEquals (opositePos $ size - 1) (size + 1)
+			assertEquals (opositePos $ size + 1) (size - 1)
+			assertEquals (opositePos $ size + size) 0
 
 		assertEquals :: (Show a, Eq a) => a -> a -> IO ()
 		assertEquals x y | x == y = putStr "."
